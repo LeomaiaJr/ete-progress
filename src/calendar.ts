13d6ba245275a6cc160da32879dd9263383b1af5
Rodrigo Addor <rodrigotladdor@gmail.com>
2019-02-01 13:50:25 -0200

    Moved whitelist and blacklist to separate file, added exports and imports

--- src/calendar.ts
+++ src/calendar.ts
@@ -1,54 +1,6 @@
-class Day {
-	constructor(public month: number, public day: number) {
-	}
+import {whitelist, blacklist} from './lists'
 
-	static fromTuple(tuple: number[]): Day {
-		return new Day(tuple[0], tuple[1])
-	}
-}
-
-class Days {
-	constructor(public month: number, public dayStart: number, public dayEnd: number) {
-	}
-
-	static fromTuple(tuple: number[]): Days {
-		return new Days(tuple[0], tuple[1], tuple[2])
-	}
-}
-
-const Whitelist: Array<Day|Days> = [
-	[2, 9],
-	[2, 23],
-	[3, 23],
-	[4, 6],
-	[5, 18],
-	[6, 1],
-	[6, 29],
-	[7, 6],
-	[8, 17],
-	[8, 31],
-	[9, 14],
-	[9, 28],
-	[10, 5],
-	[10, 19],
-	[11, 9],
-].map(e => e.length === 2 ? Day.fromTuple(e) : Days.fromTuple(e))
-
-const Blacklist: Array<Day|Days> = [
-	[3, 4, 8],
-	[4, 18, 19],
-	[5, 1],
-	[5, 22, 24],
-	[6, 20, 21],
-	[7, 15, 29],
-	[10, 7, 11],
-	[11, 15],
-].map(e => e.length === 2 ? Day.fromTuple(e) : Days.fromTuple(e))
-
-class Calendar {
-	private static whitelist = Whitelist
-	private static blacklist = Blacklist
-
+export default class Calendar {
 	private static isSameDay(date: Date, day: Day): boolean {
 		return date.getUTCDate() == day.day && date.getUTCMonth() + 1 == day.month
 	}
@@ -59,7 +11,7 @@ class Calendar {
 		return month === days.month && day >= days.dayStart && day <= days.dayEnd
 	}
 
-	static isInList(date: Date, list: Array<Day|Days>) {
+	static isInList(date: Date, list: Array<Day | Days>) {
 		return list.every(value => {
 			if (value instanceof Day)
 				return this.isSameDay(date, value)
@@ -73,6 +25,24 @@ class Calendar {
 	}
 
 	static isSchoolDay(day: Date): boolean {
-		return this.isWeekend(day) && !this.isInList(day, this.whitelist) ? false : !this.isInList(day, this.blacklist)
+		return this.isWeekend(day) && !this.isInList(day, whitelist) ? false : !this.isInList(day, blacklist)
+	}
+}
+
+export class Day {
+	constructor(public month: number, public day: number) {
+	}
+
+	static fromTuple(tuple: number[]): Day {
+		return new Day(tuple[0], tuple[1])
+	}
+}
+
+export class Days {
+	constructor(public month: number, public dayStart: number, public dayEnd: number) {
+	}
+
+	static fromTuple(tuple: number[]): Days {
+		return new Days(tuple[0], tuple[1], tuple[2])
 	}
 }
