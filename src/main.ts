3a30d361fbdfa67e9294f47497300563526b26fc
Rodrigo Addor <rodrigotladdor@gmail.com>
2019-02-05 20:13:35 -0200

    Added Express backend, moved tweeting to /api/post

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import Calendar from './calendar'
-import createProgressIndicator from './progress_indicator'
-import client from './client'
-
-let calendar: Calendar
+import * as server from './server'
 
 async function build() {
 	let data
@@ -12,26 +9,7 @@ async function build() {
 		throw new Error(`Data '${process.env.DATA}' not found`)
 	}
 
-	calendar = Calendar.fromModule(data)
+	return Calendar.fromModule(data)
 }
 
-function run() {
-	if (!calendar.isSchoolDay(new Date())) {
-		console.log('There are no classes today, quitting.')
-		process.exit()
-	}
-
-	const schoolDaysAmount = calendar.countSchoolDays()
-	const todaySchoolDay = calendar.getSchoolDayNumber()
-	const progressBar = createProgressIndicator(todaySchoolDay / schoolDaysAmount, {length: 15})
-	const daysRemaining = Math.round(Math.abs((calendar.endDay.date.getTime() - new Date().getTime()) / (24 * 60 * 60 * 1000)))
-
-	const message = `${Math.floor(todaySchoolDay / schoolDaysAmount * 100)}%    ${progressBar}\nFaltam ${daysRemaining} dias (${schoolDaysAmount - todaySchoolDay} letivos	)`
-
-	console.log(`Posting message: '${message}'`)
-	client.post('statuses/update', {status: message}, (error) => {
-		if (error) throw error
-	})
-}
-
-build().then(run)
+build().then(calendar => server.start(calendar, parseInt(process.env.PORT)))
---
+++ src/server.ts
@@ -0,0 +1,42 @@
+import express = require('express')
+import Calendar from './calendar'
+import createProgressIndicator from './progress_indicator'
+import client from './client'
+
+let calendar: Calendar
+
+const app = express()
+app.use(express.static('../public'))
+
+app.get('/api/percent', (req, res) => {
+	let date = new Date(parseInt(req.query.millis) || Date.now())
+	console.log(date)
+	res.send(JSON.stringify({
+		percent: calendar.getSchoolDayNumber(date) / calendar.countSchoolDays(),
+	}))
+})
+
+app.get('/api/post', (req, res) => {
+	if (!calendar.isSchoolDay(new Date())) {
+		res.send('There are no classes today, quitting.')
+	}
+
+	const schoolDaysAmount = calendar.countSchoolDays()
+	const todaySchoolDay = calendar.getSchoolDayNumber()
+	const progressBar = createProgressIndicator(todaySchoolDay / schoolDaysAmount, {length: 15})
+	const daysRemaining = Math.round(Math.abs((calendar.endDay.date.getTime() - new Date().getTime()) / (24 * 60 * 60 * 1000)))
+
+	const message = `${Math.floor(todaySchoolDay / schoolDaysAmount * 100)}%    ${progressBar}\nFaltam ${daysRemaining} dias (${schoolDaysAmount - todaySchoolDay} letivos	)`
+
+	res.send(`Posting message: '${message}'`)
+	if (typeof req.query.debug !== 'undefined' && !req.query.debug) {
+		client.post('statuses/update', {status: message}, (error) => {
+			if (error) throw error
+		})
+	}
+})
+
+export function start(data: Calendar, port: number = 80) {
+	calendar = data
+	app.listen(port)
+}
